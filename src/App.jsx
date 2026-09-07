207990714d688b1f0d8aa8631e06afad9e67360e
Ayush Thakur <ayushtha922@gmail.com>
2026-09-07 16:40:17 +0530

    Initial implementation of Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Dashboard from './assets/Components/Dashboard/Dashboard'
 import './App.css'
+import Login from './assets/Components/Authentication/Login/Login'
 
 function App() {
   return (
     <>
       <Dashboard />
+      <Login/>
     </>
   )
 }
